c36230fcc2e48b1af5a8195233d0d7e93f84e495
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-17 12:15:37 +0200

    changing styles and basic structure of app

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from 'react';
+import logo from './logo.svg';
+import { FaTimes } from 'react-icons/fa';
+import { social, links } from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <aside className={`sidebar`}>
+      <div className='sidebar-header'>
+        <img src={logo} className='logo' alt='coding addict' />
+        <button className='close-btn' title="Close">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url} title={text}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((link) => {
+          const { id, url, icon, title } = link;
+          return (
+            <li key={id}>
+              <a href={url} title={title} target='_blank'>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/data.js
+++ src/data.js
@@ -15,31 +15,31 @@ export const links = [
   {
     id: 1,
     url: '/',
-    text: 'home',
+    text: 'Home',
     icon: <FaHome />,
   },
   {
     id: 2,
     url: '/team',
-    text: 'team',
+    text: 'Team',
     icon: <FaUserFriends />,
   },
   {
     id: 3,
     url: '/projects',
-    text: 'projects',
+    text: 'Projects',
     icon: <FaFolderOpen />,
   },
   {
     id: 4,
     url: '/calendar',
-    text: 'calendar',
+    text: 'Calendar',
     icon: <FaCalendarAlt />,
   },
   {
     id: 5,
     url: '/documents',
-    text: 'documents',
+    text: 'Documents',
     icon: <FaWpforms />,
   },
 ];
@@ -49,25 +49,30 @@ export const social = [
     id: 1,
     url: 'https://www.twitter.com',
     icon: <FaFacebook />,
+    title: 'Facebook'
   },
   {
     id: 2,
     url: 'https://www.twitter.com',
     icon: <FaTwitter />,
+    title: 'Twitter'
   },
   {
     id: 3,
     url: 'https://www.twitter.com',
     icon: <FaLinkedin />,
+    title: 'LinkedIn'
   },
   {
     id: 4,
     url: 'https://www.twitter.com',
     icon: <FaBehance />,
+    title: 'Behance'
   },
   {
     id: 5,
     url: 'https://www.twitter.com',
     icon: <FaSketch />,
+    title: 'Sketch'
   },
 ];
